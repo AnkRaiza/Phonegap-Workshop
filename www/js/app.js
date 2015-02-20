428c43e63e79d19146aa667016c51f12001a647d
Pier Castaneda <pier_1_2_3@hotmail.com>
2015-02-20 15:59:17 -0500

    Css and templates
    Add css and templates to the project

--- www/js/app.js
+++ www/js/app.js
@@ -3,40 +3,39 @@
 
     /* ---------------------------------- Local Variables ---------------------------------- */
     var service = new EmployeeService();
+    var homeTpl = Handlebars.compile($("#home-tpl").html());
+    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
     service.initialize().done(function () {
-        console.log("Service initialized");
+        renderHomeView();
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
     document.addEventListener('deviceready', function () {
-	if (navigator.notification) { // Override default HTML alert with native dialog
-			window.alert = function (message) {
-				navigator.notification.alert(
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
 				message,    // message
 				null,       // callback
 				"Workshop", // title
 				'OK'        // buttonName
 				);
-			};
-		}
-	FastClick.attach(document.body);
-	}, false);
-	$('.search-key').on('keyup', findByName);
-    $('.help-btn').on('click', function() {
+            };
+        }
+        FastClick.attach(document.body);
+    }, false);
+    $('.help-btn').on('click', function () {
         alert("Employee Directory v3.4");
     });
 
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
         service.findByName($('.search-key').val()).done(function (employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.content').html(employeeListTpl(employees));
         });
     }
 
+    function renderHomeView() {
+        $('body').html(homeTpl());
+        $('.search-key').on('keyup', findByName);
+    }
 }());
