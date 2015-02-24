edef53ec19dad9ade6056c2654779ad249bab426
Pier Castaneda <pier_1_2_3@hotmail.com>
2015-02-24 13:59:51 -0500

    test
    test direct approacg

--- www/js/EmployeeView.js
+++ www/js/EmployeeView.js
@@ -71,18 +71,18 @@ var EmployeeView = function (employee) {
         event.preventDefault();
         $('.videoPlayerDiv').show();
         var player = document.getElementById("videoPlayer");
-        var source = document.createElement('source');        
+        //var source = document.createElement('source');        
         //var filename = "small.mp4";
         //var videoURL = "http://techslides.com/demos/sample-videos/small.mp4";
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
-            var rootDirectory = fileSystem.root.toURL();
-            source.setAttribute('src', rootDirectory + 'sdcard/Download/small.mp4');
-            video.appendChild(source);
-            //document.getElementById("videoPlayer").src = rootDirectory + "sdcard/Download/small.mp4";
-        }, function () {
-            alert("fails!");
-        });
-        player.load();
+        //window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
+        //    var rootDirectory = fileSystem.root.toURL();
+        //    source.setAttribute('src', rootDirectory + 'sdcard/Download/small.mp4');
+        //    video.appendChild(source);
+        //    //document.getElementById("videoPlayer").src = rootDirectory + "sdcard/Download/small.mp4";
+        //}, function () {
+        //    alert("fails!");
+        //});
+        //player.load();
         player.play();
         //requestFileSystem(PERSISTENT, 0, function (fileSystem) {
         //    var ft = new FileTransfer();
