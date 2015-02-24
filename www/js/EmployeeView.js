5910a11bfbda8412bf35a5f8a45c8502ab283115
Pier Castaneda <pier_1_2_3@hotmail.com>
2015-02-24 14:23:04 -0500

    a
    a

--- www/js/EmployeeView.js
+++ www/js/EmployeeView.js
@@ -71,20 +71,21 @@ var EmployeeView = function (employee) {
         event.preventDefault();
         $('.videoPlayerDiv').show();
         var player = document.getElementById("videoPlayer");
-        alert("fileSystem.root.toURL()=" + fileSystem.root.toURL());
-        alert("fileSystem.root.toInternalURL()=" + fileSystem.root.toInternalURL());
-        alert("fileSystem.root.nativeURL=" + fileSystem.root.nativeURL);
+        
         //var source = document.createElement('source');        
         //var filename = "small.mp4";
         //var videoURL = "http://techslides.com/demos/sample-videos/small.mp4";
-        //window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
         //    var rootDirectory = fileSystem.root.toURL();
         //    source.setAttribute('src', rootDirectory + 'sdcard/Download/small.mp4');
         //    video.appendChild(source);
-        //    //document.getElementById("videoPlayer").src = rootDirectory + "sdcard/Download/small.mp4";
-        //}, function () {
-        //    alert("fails!");
-        //});
+        //document.getElementById("videoPlayer").src = rootDirectory + "sdcard/Download/small.mp4";
+            alert("fileSystem.root.toURL()=" + fileSystem.root.toURL());
+            alert("fileSystem.root.toInternalURL()=" + fileSystem.root.toInternalURL());
+            alert("fileSystem.root.nativeURL=" + fileSystem.root.nativeURL);
+        }, function () {
+            alert("fails!");
+        });
         //player.load();
         player.play();
         //requestFileSystem(PERSISTENT, 0, function (fileSystem) {
